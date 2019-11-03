13e47e4383d692b073c49b35339d2335b9e2a7f2
Andrea Colledan <and.colledan@gmail.com>
2019-11-02 22:37:10 -0400

    Added id generation to client

--- workloadQueryClient.py
+++ workloadQueryClient.py
@@ -1,4 +1,5 @@
 import time
+import random
 import argparse
 
 import grpc
@@ -8,11 +9,21 @@ import workloadQuery_pb2_grpc
 import requests
 from tabulate import tabulate
 
-_GRPC_ENDPOINT = ''
-_HTTP_ENDPOINT = 'http://mlquery.mohammadrafee.com/v1/mldata'
+_GRPC_ENDPOINT = 'grpc.dataset.mohammadrafee.com:80'
+_HTTP_ENDPOINT = 'http://rest.dataset.mohammadrafee.com/v1/mldata'
 
 def generateId():
-    return 0
+    t = round(time.time()*1000) % 1000000
+    random.seed()
+    r = random.randint(10000,99999)
+    return int(str(r)+str(t))
+
+def valid(args):
+    if args['type'] not in ['training', 'testing']:
+        return False
+    if args['workloadMetric'] not in ['CPU', 'NetworkIn', 'NetworkOut', 'Memory']:
+        return False
+    return True
 
 def textRequest(rfwid, args):
     r = requests.get(_HTTP_ENDPOINT, params=args, headers={'rfwid':str(rfwid)})
@@ -29,22 +40,24 @@ def binaryRequest(rfwid, args):
     bt = workloadQuery_pb2.RequestForWorkload.BenchmarkType(source=args['source'], type=args['type'])
     rfw = workloadQuery_pb2.RequestForWorkload(rfwId=rfwid, benchmarkType=bt, workloadMetric=args['workloadMetric'], batchUnit=args['batchUnit'], batchId=args['batchId'], batchSize=args['batchSize'])
 
-    r = client.GetSamples(request)
+    r = client.GetSamples(rfw)
     return {'lastbatchId': r.lastBatchId, 'rfwid': r.rfwId, 'samples': r.samples}
 
 
 
 parser = argparse.ArgumentParser(description='Retrieve workload data for machine learning purposes.')
 parser.add_argument('source', metavar='<benchmark>', type=str, help='The benchmark from which the data is sourced.')
-parser.add_argument('type', metavar='<set>', type=str, help='The set (train/test) from which the data is sourced.')
-parser.add_argument('workloadMetric', metavar='<metric>', type=str, help='The metric ("cpu", "networkIn", "networkOut", "memory", "finalTarget") to retrieve for each sample.')
+parser.add_argument('type', metavar='<set>', type=str, help='The set (training/testing) from which the data is sourced.')
+parser.add_argument('workloadMetric', metavar='<metric>', type=str, help='The metric ("CPU", "NetworkIn", "NetworkOut", "Memory") to retrieve for each sample.')
 parser.add_argument('--binary', '-b', action='store_true', help='Use binary (de)serialization instead of text (de)serialization.')
 parser.add_argument('--batch-unit', '-u', metavar='<unit>', dest='batchUnit', type=int, default=32, help='The number of samples included in each batch.')
 parser.add_argument('--batch-id', '-i', metavar='<id>', dest='batchId', type=int, default=1, help='The index of the first batch to retrieve.')
 parser.add_argument('--batch-size', '-s', metavar='<size>', dest='batchSize', type=int, default=1, help='The number of batches to retrieve.')
 
 args = vars(parser.parse_args())
-print(args)
+if not valid(args):
+    print('Invalid argument(s), execute with --help to review supported arguments.')
+    exit()
 
 rfwId = generateId()
 
